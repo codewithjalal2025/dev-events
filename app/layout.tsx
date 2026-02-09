66c2a3166ef30d3f926a8ffd36385e174708d232
jalaluddin sahak <jalaluddinsahak5@gmail.com>
2026-02-09 11:09:39 +0430

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const martianMono = Martian_Mono({
 
 export const metadata: Metadata = {
   title: "Dev Event",
-  description: "The Hub for Every Dev Event You Musin't Miss",
+  description: "The Hub for Every Dev Event You Musin't Miss look this website",
 };
 
 export default function RootLayout({
