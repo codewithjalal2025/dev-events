29f1a3de56313f49a876fa185ffd5ee43c844d86
jalaluddin sahak <jalaluddinsahak5@gmail.com>
2026-02-09 10:39:59 +0430

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,59 @@
+import type { Metadata } from "next";
+import {  Martian_Mono, Schibsted_Grotesk } from "next/font/google";
+import "./globals.css";
+import LightRays from "@/components/LightRays";
+import Navbar from "@/components/Navbar";
+
+const schibstedGrotesk = Schibsted_Grotesk({
+  variable: "--font-schibsted-grotesk",
+  subsets: ["latin"],
+});
+
+const martianMono = Martian_Mono({
+  variable: "--font-martian-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Dev Event",
+  description: "Dev Event is the best Next js 16 Application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={` min-h-screen ${schibstedGrotesk.variable} ${martianMono.variable} antialiased`}
+      >
+        <Navbar/>
+      
+<div className=" absolute inset-0 top-0 z-[-1] min-h-screen">
+
+  <LightRays
+    raysOrigin="top-center-offset"
+    raysColor="#5dfeca"
+    raysSpeed={0.5}
+    lightSpread={0.9}
+    rayLength={1.4}
+    followMouse={true}
+    mouseInfluence={0.1}
+    noiseAmount={0}
+    distortion={0.02}
+    className="custom-rays"
+    pulsating={false}
+    fadeDistance={1}
+    saturation={1}
+/>
+    </div>
+    
+    <main>
+    {children}
+    </main>
+      </body>
+    </html>
+  );
+}
---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import Image from "next/image"
+
+const ExploreBtn = () => {
+  return (
+   <button type="button" id="explore-btn" className='mt-7 mx-auto' onClick={()=>console.log("clicked")}>
+    <a href="#events">
+      ExploreBtn
+      <Image
+       src="/icons/arrow-down.svg"
+       alt="Arrow-down"
+       width={24}
+       height={24}
+       
+       />
+    </a>
+   </button>
+  )
+}
+
+export default ExploreBtn
